9f39e35532e3bcbbfb1099277dfc1d8092d563e7
Paola Sarmiento <paolasarmiento@pop-os.localdomain>
2020-08-11 08:56:02 -0500

    Cambios Database

--- src/post.js
+++ src/post.js
@@ -1,9 +1,27 @@
 const formPost = document.getElementById("formPost");
 const header = document.getElementById("header");
 const footer = document.getElementById("footer");
+const sendPost = document.getElementById("sendPost");
+
+sendPost.addEventListener("click", formPostView);
 
 export const showPostAfterLogin = () => {
   formPost.style.display = "flex";
   header.style.display = "none";
   footer.style.display = "none";
 };
+
+const saveDataPost = (placePost, descriptionPost) =>
+  store.collection("userCollection").doc().set({
+    placePost: placePost,
+    descriptionPost: descriptionPost,
+  });
+
+async function formPostView() {
+  const placePost = document.getElementById("placePost").value;
+  const descriptionPost = document.getElementById("descriptionPost").value;
+
+  await saveDataPost(placePost, descriptionPost);
+
+  console.log(placePost, descriptionPost);
+}
