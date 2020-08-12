34304138968cb17dfcc0fd7f41c05a48a4593dbe
karlaguaita <66104641+karlaguaita@users.noreply.github.com>
2020-08-11 20:10:04 -0500

    firestorage

--- src/post.js
+++ src/post.js
@@ -2,6 +2,7 @@ const formPost = document.getElementById("formPost");
 const header = document.getElementById("header");
 const footer = document.getElementById("footer");
 const sendPost = document.getElementById("sendPost");
+const invalidPost = document.getElementById("invalidPost");
 
 sendPost.addEventListener("click", formPostView);
 
@@ -11,17 +12,40 @@ export const showPostAfterLogin = () => {
   footer.style.display = "none";
 };
 
-const saveDataPost = (placePost, descriptionPost) =>
-  store.collection("userCollection").doc().set({
+const saveDataPost = (placePost, descriptionPost) => {
+  const docData = store.collection("userCollection").doc().set({
     placePost: placePost,
     descriptionPost: descriptionPost,
   });
+};
 
-async function formPostView() {
-  const placePost = document.getElementById("placePost").value;
-  const descriptionPost = document.getElementById("descriptionPost").value;
+const collectionImagePost = (imgPost) => {
+  const storageRef = storage.ref();
+  const imagesRef = storageRef.child("images");
+  const fileName = imgPost;
+  const spaceRef = imagesRef.child(fileName);
+    
+  };
+  console.log("subio");
 
-  await saveDataPost(placePost, descriptionPost);
+ function formPostView() {
+  const placePost = document.querySelector("#placePost").value;
+  const descriptionPost = document.querySelector("#descriptionPost").value;
+  const imgPost = document.querySelector("#imgPost").value;
+ 
+  if (placePost == null || placePost.length == 0 || /^\s+$/.test(placePost)) {
+    invalidPost.innerHTML = "You must enter a valid place";
+    return false;
+  }
+
+  if (descriptionPost == null || descriptionPost.length == 0 || /^\s+$/.test(descriptionPost)) {
+    invalidPost.innerHTML = "You must enter a valid description place";
+    return false;
+  }
+
+  saveDataPost(placePost, descriptionPost);
 
   console.log(placePost, descriptionPost);
 }
+
+
