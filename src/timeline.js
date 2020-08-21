e731eed31ae04d22d51aa0dd44ab25d65e1fa21c
Paola Sarmiento <paolasarmiento@pop-os.localdomain>
2020-08-21 16:57:28 -0500

    ultimos cambios del viernes

--- src/timeline.js
+++ src/timeline.js
@@ -26,9 +26,7 @@ export const loadTimeline = async () => {
   const onGetTask = (callback) =>
     store.collection("userPostsCollection").onSnapshot(callback);
 
-  console.log("antes");
   const querySnapshot = await store.collection("userPostsCollection").get();
-  console.log("despues");
 
   //onGetTask((querySnapshot) => {
   sectionTimeline.innerHTML = `<div id="headLogoUserContainer" class="head-logo-user-container">
@@ -42,13 +40,13 @@ export const loadTimeline = async () => {
   </div>
 </div>`;
 
-  let numberLikesCounter = 0;
+  // let numberLikesCounter = 0;
 
   querySnapshot.forEach((doc) => {
     const cardPost = doc.data();
-    const likeOnId = "likeOn" + numberLikesCounter;
-    const likeOffId = "likeOff" + numberLikesCounter;
-    const dotsID = "dots" + numberLikesCounter;
+    const likeOnId = "likeOn";
+    const likeOffId = "likeOff";
+    const dotsID = "dots";
 
     sectionTimeline.innerHTML += `
           <div id="postTimelineContainer" class="post-timeline-container">
@@ -58,8 +56,8 @@ export const loadTimeline = async () => {
                 <span>userNameProfile</span>
               </div>
               <div id="editDots" class="edit-dots">
-                <p id="dots" class="dots" onclick="DotsMenu(${dotsID})"><strong>...</strong></p>
-                <div class="dropdown-content" id="${dotsID}" onclick="report()">
+                <button id="dots" class="dots" onclick="DotsMenu(${dotsID})"><strong>...</strong></button>
+                <div class="dropdown-content" id="${dotsID}" onclick="report()" style="display: none;">
                   <a href="#">Report</a>
                 </div>
                 
@@ -74,7 +72,7 @@ export const loadTimeline = async () => {
                 <p id="descriptionCardDate">${cardPost.dateImg}</p>
                 <p>${cardPost.descriptionPost}</p>
               <div class="container-like">
-                <img src="/img/LikeOff.png" alt="LikeOff" class="like-off" id="${likeOffId}">
+                <img src="/img/LikeOff.png" alt="LikeOff" class="like-off" onclick="likePost(${likeOffId})">
                 <img src="/img/Likeon.png" alt="LikeOn" class="like-on" id="${likeOnId}">
                 <p id="numbersLikes${numberLikesCounter}"></p>
               </div>
@@ -86,28 +84,15 @@ export const loadTimeline = async () => {
             </div>
           </div>
           `;
-    document.getElementById(likeOffId).onclick = function () {
-      likePost(likeOffId, likeOnId);
-    };
-    document.getElementById(likeOnId).onclick = function () {
-      unLikePost(likeOffId, likeOnId);
-    };
-    numberLikesCounter += 1;
+    // console.log(document.getElementById(likeOffId));
+    // document.getElementById(likeOffId).click = function () {
+    //   likePost(likeOffId, likeOnId);
+    // };
+    // document.getElementById(likeOnId).onclick = function () {
+    //   unLikePost(likeOffId, likeOnId);
+    // };
+    // numberLikesCounter += 1;
   });
-
-  // const likeOffImgs = document.querySelectorAll(".like-off-img-select");
-  // const likeOnImgs = document.querySelectorAll(".like-on-img-select");
-
-  // document.getElementById("likeOn0").onclick = test;
-  // document.getElementById("likeOff0").onclick = test;
-
-  // likeOffImgs.forEach((img) => {
-  //   img.onclick = test;
-  // });
-
-  // likeOnImgs.forEach((img) => {
-  //   img.onclick = test;
-  // });
 
   //este botón se debe incluir y editar para ponerlo dentro del menú
   sectionTimeline.innerHTML += `<input id="testIrPost" type="button" value="Ir a post" />`;
